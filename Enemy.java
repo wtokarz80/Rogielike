1bdf511767bd4439b865138c451d01c87d4d1da2
Huberkrok <hubh1234@wp.pl>
2020-04-23 22:33:34 +0200

    Fixed bugs with healing

--- Enemy.java
+++ Enemy.java
@@ -35,7 +35,7 @@ public class Enemy extends Character {
             System.out.println("What you want to do ? \n1.Attack \n" + "2.Defence \n3.Try to escape");
             Scanner action = new Scanner(System.in);
             String actions = action.nextLine();
-    
+
             switch (actions) {
                 case "1":
                     System.out.println("You attacked for: " + (player.getStats().attack - bandit.def) + "damage");
@@ -46,24 +46,28 @@ public class Enemy extends Character {
                         fight = false;
                         break;
                     }
-                    player.getStats().currentHP -= bandit.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
+                    if (bandit.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= bandit.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
                         System.out.println("You died");
                         fight = false;
                         break;
                     }
-                    break;
-    
+                }
+                break;
+
                 case "2":
                     player.getStats().def += 1;
                     bonusDef += 1;
-                    player.getStats().currentHP -= bandit.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
+                    if (bandit.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= bandit.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
                         System.out.println("You died");
                         fight = false;
                         break;
                     }
-                    break;
+                }
+                break;
                 case "3":
                     String[] escape = { "Escaped", "Escaped", "Escaped", "Failed" };
                     String result = escape[(int) (Math.random() * escape.length)];
@@ -73,16 +77,19 @@ public class Enemy extends Character {
                         break;
                     }
                     System.out.println("You failed to escape");
-                    player.getStats().currentHP -= bandit.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
+                    if (bandit.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= bandit.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
                         System.out.println("You died");
                         fight = false;
                         break;
                     }
             }
+            break;
         }
         player.getStats().def -= bonusDef;
     }
+}
 
     public void ghostFight(Player player) {
         Statistics ghost = new Statistics(1, 0, 5, 5, 5, 5, 10, 5);
@@ -95,7 +102,7 @@ public class Enemy extends Character {
             System.out.println("What you want to do ? \n1.Attack \n" + "2.Defence \n3.Try to escape");
             Scanner action = new Scanner(System.in);
             String actions = action.nextLine();
-    
+
             switch (actions) {
                 case "1":
                     System.out.println("You attacked for: " + (player.getStats().attack - ghost.def) + "damage");
@@ -106,14 +113,16 @@ public class Enemy extends Character {
                         fight = false;
                         break;
                     }
-                    player.getStats().currentHP -= ghost.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
+                    if (ghost.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= ghost.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
                         System.out.println("You died");
                         fight = false;
                         break;
                     }
-                    break;
-    
+                }
+                break;
+
                 case "2":
                     player.getStats().def += 1;
                     bonusDef += 1;
@@ -133,12 +142,16 @@ public class Enemy extends Character {
                         break;
                     }
                     System.out.println("You failed to escape");
-                    player.getStats().currentHP -= ghost.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
-                        System.out.println("You died");
-                        fight = false;
-                        break;
+                    if (ghost.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= ghost.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
+                            System.out.println("You died");
+                            fight = false;
+                            break;
+                        }
                     }
+                    break;
+
             }
         }
         player.getStats().def -= bonusDef;
@@ -166,24 +179,29 @@ public class Enemy extends Character {
                         fight = false;
                         break;
                     }
-                    player.getStats().currentHP -= ogre.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
+                    if (ogre.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= ogre.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
                         System.out.println("You died");
                         fight = false;
                         break;
                     }
                     break;
+                }
+                break;
 
                 case "2":
                     player.getStats().def += 1;
                     bonusDef += 1;
-                    player.getStats().currentHP -= ogre.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
+                    if (ogre.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= ogre.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
                         System.out.println("You died");
                         fight = false;
                         break;
                     }
-                    break;
+                }
+                break;
                 case "3":
                     String[] escape = { "Escaped", "Escaped", "Escaped", "Failed" };
                     String result = escape[(int) (Math.random() * escape.length)];
@@ -193,12 +211,15 @@ public class Enemy extends Character {
                         break;
                     }
                     System.out.println("You failed to escape");
-                    player.getStats().currentHP -= ogre.attack - player.getStats().def;
-                    if (player.getStats().currentHP <= 0) {
+                    if (ogre.attack - player.getStats().def >= 0) {
+                        player.getStats().currentHP -= ogre.attack - player.getStats().def;
+                        if (player.getStats().currentHP <= 0) {
                         System.out.println("You died");
                         fight = false;
                         break;
                     }
+                }
+                break;
             }
         }
         player.getStats().def -= bonusDef;
